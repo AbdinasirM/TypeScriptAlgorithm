e6468192da820585cd0e571804f3ff546fae1945
Abdi <aabdi5520@gmail.com>
2024-01-14 09:15:00 -0600

    added array examples

---
+++ Basics/arrays/arrayExample.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+
+
+    //take user input for the dynamic array:
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    
+    
+    //static  array:
+    int myarray[5];
+
+    //ading elements to the array
+    for(int i= 0;i<5;i++){
+        myarray[i] = i;
+        cout<<"Static Array: "<<myarray[i]<<endl;
+    }
+
+
+
+    //dynamic array:
+    vector<int> myvector;
+    for(int i=0;i<num;i++){
+        myvector.push_back(i);
+        cout<<"Dynamic array: "<<myvector[i]<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
